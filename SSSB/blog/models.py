c66cfdc8e4b8705a3666e86a7bb09b733c521c56
Arbusz <arbuszg@gmail.com>
2017-11-17 15:50:08 +0800

    first commit for blog

---
+++ SSSB/blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    # 文章标题
+    title = models.CharField(max_length=70)
+    # 文章正文
+    body = models.TextField()
+    # 创建时间和最后修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    # 文章与标签对应
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    # 文章作者
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
